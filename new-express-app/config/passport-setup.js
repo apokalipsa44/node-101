ddea4e3b603350f60d80a4802b115f2639496d0e
apokalipsa44 <m_lachowicz@wp.pl>
2020-08-26 16:55:44 +0200

    passport google strategy config done

---
+++ new-express-app/config/passport-setup.js
@@ -0,0 +1,14 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("../keys/keys");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.GOOGLE_CLIENT_ID,
+      clientSecret: keys.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    () => {}
+  )
+);
